3b13a43013fa47130b019568cad2b92d63aa9511
daniel1775 <danilog1000@gmail.com>
2025-09-27 18:37:01 -0500

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Daniel on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
