b4ad5962405266e582c1deac6fbd83f0edc1b2bb
daniel1775 <danilog1000@gmail.com>
2025-10-04 20:03:51 -0500

    feat: ugly bg added to the ugly card

--- notes/ContentView.swift
+++ notes/ContentView.swift
@@ -3,17 +3,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .center) {
-            Image("kratos")
-                .resizable()
-                .frame(height: 400)
-                .cornerRadius(200)
-            Button("Press here to play") {
-                print("Loading game...")
+        ZStack(alignment: Alignment.center) {
+            Color.blue
+            VStack(alignment: .center) {
+                Image("kratos")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(200)
+                Button("Press here to play") {
+                    print("Loading game...")
+                }
+                    .padding(15)
+                    .foregroundStyle(Color.white)
+                    .background(Color.green)
             }
-                .padding(15)
-                .foregroundStyle(Color.white)
-                .background(Color.green)
+            .frame(width: 300, height: 300)
+            .background(Color.yellow)
+            .cornerRadius(20)
         }
     }
 }
